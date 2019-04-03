d4603d44b839751b3d8e827fbbd1a5c74c6fc156
Esvin González <usac.ing.esvin@gmail.com>
2019-04-02 18:22:49 -0600

    Agregando ejercicios en clase.

--- AgendaWeb/src/main/java/com/academik/agendaweb/ServletContactos.java
+++ AgendaWeb/src/main/java/com/academik/agendaweb/ServletContactos.java
@@ -24,13 +24,25 @@ public class ServletContactos extends HttpServlet {
     private static List<Contacto> contactos = new ArrayList<>();
 
     static {
-        contactos.add(new Contacto("esvux"));
-        contactos.add(new Contacto("aniras"));
-        contactos.add(new Contacto("juanpa"));
-        contactos.add(new Contacto("emersssson"));
-        contactos.add(new Contacto("sarahi"));
+        contactos.add(new Contacto("esvux", "Esvin Gonzalez", "eesvux@gmail.com"));
+        contactos.add(new Contacto("aniras", "Sarina Bolaños", "aniras@gmail.com"));
+        contactos.add(new Contacto("juanpa", "Juan Pablo", "jp@correo.com"));
+        contactos.add(new Contacto("emersssson", "Emerson Velasquez", "mecho@correo.com"));
+        contactos.add(new Contacto("sarahi", "Sara Chajon", "saraiiii@correo.com"));
     }
 
+    /*
+     * 1) Modificar el metodo doGet para mostrar TODA la informacion de los 
+     * contactos que estan en el sistema en una tabla.
+     <table>
+        <tr>
+            <th>Nickname</th>
+            <th>Fullname</th>
+            <th>email</th>
+        </tr>
+        <!--Aqui iria la info de los contactos-->
+     </table>
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try (PrintWriter out = resp.getWriter()) {
@@ -43,18 +55,30 @@ public class ServletContactos extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try (BufferedReader reader = req.getReader()) {
-            JsonReader jsonReader = Json.createReader(reader);
-            JsonObject jsonObject = jsonReader.readObject();
-            Contacto nuevo = new Contacto(
-                jsonObject.getString("nickname"),
-                jsonObject.getString("fullname"),
-                jsonObject.getString("email")
-            );
-            System.err.println("Adding " + nuevo.getNickname());
+    protected void doPost(
+            HttpServletRequest req, 
+            HttpServletResponse resp
+    ) throws ServletException, IOException {
+        try (BufferedReader buffer = req.getReader()) {
+            JsonReader reader = Json.createReader(buffer);
+            JsonObject jsonObject = reader.readObject();
+            String nuevoNickname = jsonObject.getString("nickname");
+            String nuevoFullname = jsonObject.getString("fullname");
+            String nuevoEmail = jsonObject.getString("email");
+            Contacto nuevo = new Contacto(nuevoNickname, nuevoFullname, nuevoEmail);
             contactos.add(nuevo);
+            System.err.println("Se agrego: " + nuevoNickname);
         }
     }
+    
+    /**
+     * 2) Sobrescribir el metodo doPut para modificar un recurso ya existente,
+     * la data a recibir sera:
+     * - Por medio de la URL el 'nickname' del elemento a modificar
+     * - En el cuerpo de la peticion el nuevo contenido
+     * - Buscar el elemento por medio del nickname
+     *     - Si no existe responder con un 404
+     *     - Si se encuentra, reemplazarlo por el nuevo elemento
+     */
 
 }
