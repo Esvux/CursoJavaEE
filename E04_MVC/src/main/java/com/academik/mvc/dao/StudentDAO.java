3ec9f7e36616968ed5dbf7154bba0c9a708a5001
Esvin González <egonzalez@nabenik.com>
2019-05-21 17:51:39 -0600

    Agregando el cuerpo del método 'queryAll' en 'StudentDAO'.

--- E04_MVC/src/main/java/com/academik/mvc/dao/StudentDAO.java
+++ E04_MVC/src/main/java/com/academik/mvc/dao/StudentDAO.java
@@ -1,6 +1,11 @@
 package com.academik.mvc.dao;
 
 import com.academik.mvc.model.Student;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,7 +17,25 @@ public class StudentDAO implements GeneralDAO<Student> {
 
     @Override
     public List<Student> queryAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Student> temp = new ArrayList<>();
+        try {
+            Connection conn = CONN_WRAPPER.getConnection();
+            PreparedStatement stmn = conn.prepareStatement("SELECT code, first_name, last_name, gender, email, contact_phone, guardian, birthday FROM student");
+            ResultSet result = stmn.executeQuery();
+            while(result.next()) {
+                Student s = new Student();
+                s.setCode(result.getLong("code"));
+                s.setFirstName(result.getString("first_name"));
+                s.setLastName(result.getString("last_name"));
+                s.setEmail(result.getString("email"));
+                s.setGuardian(result.getString("guardian"));
+                s.setContactPhone(result.getString("contact_phone"));
+                temp.add(s);
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return temp;
     }
 
     @Override
---
+++ E04_MVC/src/main/resources/db/V1__student_table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE public.student
+(
+  code integer NOT NULL,
+  first_name character varying(50) NOT NULL,
+  last_name character varying(50) NOT NULL,
+  gender character varying(1) NOT NULL,
+  email character varying(150) NOT NULL,
+  contact_phone character varying(50) NOT NULL,
+  guardian character varying(50) NOT NULL,
+  birthday timestamp NOT NULL,
+  CONSTRAINT pk_student PRIMARY KEY (code)
+);
+
