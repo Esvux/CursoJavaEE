59dd89d83eba25e209c34e886ff218ad808f96da
Esvin González <egonzalez@nabenik.com>
2019-06-25 17:47:51 -0600

    Agregando cambios para poder ver la lista de votantes.

---
+++ E03_MiniTSE/src/main/java/com/academik/minitse/beans/ListVotersBean.java
@@ -0,0 +1,32 @@
+package com.academik.minitse.beans;
+
+import com.academik.minitse.dao.VoterDAO;
+import com.academik.minitse.model.Voter;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author esvux
+ */
+@ManagedBean(name = "listVotersBean")
+@RequestScoped
+public class ListVotersBean {
+    
+    @Inject
+    VoterDAO dao;
+    
+    List<Voter> voters;
+
+    public List<Voter> getVoters() {
+        voters = dao.findAll();
+        return voters;
+    }
+
+    public void setVoters(List<Voter> voters) {
+        this.voters = voters;
+    }
+    
+}
--- E03_MiniTSE/src/main/java/com/academik/minitse/dao/VoterDAO.java
+++ E03_MiniTSE/src/main/java/com/academik/minitse/dao/VoterDAO.java
@@ -1,9 +1,11 @@
 package com.academik.minitse.dao;
 
 import com.academik.minitse.model.Voter;
+import java.util.List;
 import javax.faces.bean.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
 /**
@@ -19,6 +21,14 @@ public class VoterDAO {
     @Transactional
     public void register(Voter newVoter){
         em.persist(newVoter);
+    }
+
+    @Transactional
+    public List<Voter> findAll() {
+        TypedQuery<Voter> query = em.createQuery(
+                "SELECT v.dpi, v.firstName, v.lastName, v.height FROM Voter v", Voter.class
+        );
+        return query.getResultList();
     }
     
 }
