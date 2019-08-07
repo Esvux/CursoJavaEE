ed614b47110d41babd46c6d452b77b9d48995507
Esvin González <egonzalez@nabenik.com>
2019-08-06 18:20:53 -0600

    Agregando script para Postgres.

---
+++ E07_Security/E07_Security/src/main/database/roles_and_accounts.sql
@@ -0,0 +1,39 @@
+﻿CREATE TABLE IF NOT EXISTS role(
+   id SERIAL PRIMARY KEY,
+   role_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS account(
+   id SERIAL PRIMARY KEY,
+   email VARCHAR(40) NOT NULL,
+   username VARCHAR(15) NOT NULL,
+   password VARCHAR(255) NOT NULL,
+   firstname VARCHAR(100),
+   lastname VARCHAR(100),
+   active BOOLEAN,
+   created TIMESTAMP,
+   updated TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS account_role(
+   id SERIAL PRIMARY KEY,
+   account_id INTEGER,
+   role_id INTEGER,
+   FOREIGN KEY (account_id) REFERENCES account(id),
+   FOREIGN KEY (role_id) REFERENCES role(id)
+);
+
+CREATE OR REPLACE VIEW view_account_role AS (
+   SELECT account_role.account_id, account_role.role_id, account.username, account.password, role.role_name
+   FROM account_role, account, role
+   WHERE account_role.account_id = account.id
+   AND account_role.role_id = role.id
+);
+
+INSERT INTO role VALUES (1, 'admin'), (2, 'normal_user');
+INSERT INTO account (email, username, password, firstname, lastname, active) VALUES ('esvin@mail.com', 'esvin', '10/w7o2juYBrGMh32/KbveULW9jk2tejpyUAD+uC6PE=', 'Esvin', 'González', TRUE);
+INSERT INTO account (email, username, password, firstname, lastname, active) VALUES ('demo@mail.com', 'demo', '10/w7o2juYBrGMh32/KbveULW9jk2tejpyUAD+uC6PE=', 'Demo', 'User', TRUE);
+INSERT INTO account_role (account_id, role_id) VALUES (1, 1);
+INSERT INTO account_role (account_id, role_id) VALUES (2, 2);
+
+SELECT * FROM view_account_role;
