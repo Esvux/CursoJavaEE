e9ecf522c8203256a60c2c73755ac80d50d39ec4
Esvin González <egonzalez@nabenik.com>
2019-08-08 08:13:31 -0600

    Modificaciones leves sobre el ejemplo de seguridad con JAAS.

---
+++ E07_Security/E07_Security/src/main/java/com/academik/security/servlet/DemoGuestServlet.java
@@ -0,0 +1,16 @@
+
+package com.academik.security.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author esvux
+ */
+@WebServlet("/guest")
+public class DemoGuestServlet extends HttpServlet {
+    
+    
+    
+}
